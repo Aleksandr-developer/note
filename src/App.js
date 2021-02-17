c70db393111821675cf051feaf13cb788412decb
Aleksandr-developer <sann12346464@gmail.com>
2021-02-17 19:58:53 +0300

    test routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { Page2 } from './pages/Page2';
+import {Home} from './pages/Home'
+
 function App() {
   return (
-    <div className="container">
-      <h1>Test</h1>
-    </div>
+    <BrowserRouter>
+      <div className="container pt-4">
+        <Switch>
+          <Route path={'/'} exact component={Home} />
+          <Route path={'/page2'} exact component={Page2} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Page2.js
@@ -0,0 +1,9 @@
+import React, {Fragment} from 'react'
+
+export const Page2 = () => {
+    return (
+        <Fragment>
+            <h1>Page2</h1>
+        </Fragment>
+    )
+}
